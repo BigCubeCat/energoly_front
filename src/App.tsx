41a82b4861b491e6e0f42ccbc55ea62d4a01b529
BigCubeCat <bigcubecat@yandex.ru>
2022-04-10 13:09:13 +0700

    add forms (not finished)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SingIn from './forms/SingIn';
+import SingUp from './forms/SingUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SingUp />
     </div>
   );
 }
